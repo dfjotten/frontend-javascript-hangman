84f18a515c016b92123247233267c575b1752529
dfjotten <dfjotten@gmail.com>
2021-04-26 13:07:41 +0200

    Gamelost not working yet

--- gamelogic.js
+++ gamelogic.js
@@ -1,17 +1,65 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+// Collect user input letters
+  let wordDisplayed = '';
+// Display letters in array
+  for (let i = 0; i < word.length; i++) {
+
+    const guessedLetter = guesses.includes(word[i]);
+
+    if(guessedLetter) {
+      const toAdd = word[i] + ' ';
+      wordDisplayed += toAdd;
+    } else {
+      wordDisplayed += '_ ';
+    }
+  }
+// Keep count of correct letters
+// Display correct letters in String
+  return wordDisplayed;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  // input correctly guessed letters
+  for (let i = 0; i < word.length; i++) {
+    if(!guesses.includes(word[i])) {
+      return false;
+    }
+    return true;
+  }
+  //If work guessed correctly, win condition.
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  // Input wrong guesses thus far
+  for (let i = 0; i < word.length; i++) {
+    let wrongLetter = 0;
+    let gameLost = false;
+    if(!guesses.includes(word[i])) {
+      wrongLetter++;
+    }
+    // If wrong guesses === 7, game is lost
+    if(wrongLetter === 7) {
+      return gameLost = true;
+    }
+    return gameLost = false;
+  }
+}
+
+function isSingleLetter(letter) {
+  //set condition for letter being 1 length
+  if (letter.length > 1 || letter.length < 1) {
+    return false;
+    // connect msg to say user can only guess 1 letter
+    // Block 2 or more letter guess from counting as wrong guess
+  } else {
+    return true;
+    // accept as guess
+  }
 }
 
 module.exports = {
   displayWordSoFar: displayWordSoFar,
   isGameWon: isGameWon,
   isGameLost: isGameLost,
+  isSingleLetter: isSingleLetter,
 };
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const { question } = require("readline-sync");
-const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
+const { displayWordSoFar, isGameWon, isGameLost, isSingleLetter } = require("./gamelogic");
+
+// Function to ask user for letter
+// function askForLetter() {
+//   let letter = question("Vul in een letter: ")
+//   return letter;
+// }
 
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
+  console.log(displayWordSoFar(word,guesses));
+
+  if (isGameWon(word,guesses)) {
+    console.log("Gefeliciteerd, je hebt gewonnen!");
+    return;
+  }
+
+  if (isGameLost(word,guesses)) {
+    console.log("Helaas, je hebt 7 letters fout geraden en het spel verloren");
+    return;
+  }
 
   const letter = question("Raad een letter: ");
+  if (!isSingleLetter(letter)) {
+    console.log("Je kunt maar een letter invoeren")
+  }
 
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
